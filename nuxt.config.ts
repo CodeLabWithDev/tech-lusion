035ecc780c20a1186b5ff8b22daf71e653f97c10
devu13055 <devangpastagia8005@gmail.com>
2025-08-04 10:15:27 +0530

    feat: Update viewport meta tag and organize head links in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineNuxtConfig } from "nuxt/config";
-import { fileURLToPath } from "node:url";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
@@ -11,29 +10,33 @@ export default defineNuxtConfig({
     head: {
       title: "Tech Lusion",
       meta: [
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
         { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
         { name: "description", content: "Tech Lusion - Your Tech Haven" },
         { name: "keywords", content: "tech, blog, articles, tutorials" },
         { name: "author", content: "Tech Lusion Team" },
       ],
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Lateef:wght@200;300;400;500;600;700;800&family=Outfit:wght@100..900&display=swap",
+        },
+      ],
     },
-    pageTransition: { name: "fade", mode: "out-in" },
-    layoutTransition: { name: "fade", mode: "out-in" },
-    // baseURL: "/",
-    // buildAssetsDir: "assets/",
+    // pageTransition: { name: "fade", mode: "out-in" },
+    // layoutTransition: { name: "layout", mode: "out-in" },
   },
 
   vite: {
     plugins: [tailwindcss()],
   },
-
-  // alias: {
-  //   images: fileURLToPath(new URL("./assets/images", import.meta.url)),
-  //   style: fileURLToPath(new URL("./assets/style", import.meta.url)),
-  //   data: fileURLToPath(new URL("./assets/other/data", import.meta.url)),
-  // },
 
   modules: ["@nuxt/image"],
 });
