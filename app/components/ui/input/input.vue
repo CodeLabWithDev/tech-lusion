b116c8e8a8933c2217b7ef13caf631e8f0306458
devu13055 <devangpastagia8005@gmail.com>
2025-08-04 10:14:13 +0530

    feat(components): implement input component with email validation and model binding

--- app/components/ui/input/input.vue
+++ app/components/ui/input/input.vue
@@ -1,5 +1,26 @@
 <template>
-	<h1>Input</h1>
+  <div class="relative flex items-center bg-white rounded-full overflow-hidden">
+    <input
+      :value="modelValue"
+      @input="
+        $emit('update:modelValue', ($event.target as HTMLInputElement)?.value)
+      "
+      type="email"
+      placeholder="Enter your email"
+      required
+      class="w-full px-[24px] py-[8px] ssm:px-4 ssm:py-3 text-neutral-700 focus:outline-none placeholder:text-neutral-600 min-h-[50px] ssm:min-h-[72px]"
+    />
+    <!-- Slot for the button -->
+    <slot name="prefix" />
+  </div>
 </template>
+
 <script setup lang="ts">
+// Props
+defineProps<{
+  modelValue: string;
+}>();
+
+// Emits
+defineEmits(["update:modelValue"]);
 </script>
